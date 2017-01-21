b01a42944442200297d03630401ac5c1a58c7080
darthjee <darthjee@gmail.com>
2017-01-20 23:50:17 -0200

    Add initial spec

--- spec/lib/thorwald/exporter_spec.rb
+++ spec/lib/thorwald/exporter_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Thorwald::Exporter do
+  let(:subject) { described_class.new(Document, parameters, options) }
+  let(:options) { {} }
+  let(:parameters) { {} }
+
+  describe '#as_json' do
+    context 'when there are no documents' do
+      it 'returns the documents found as json' do
+        expect(subject.as_json).to be_empty
+      end
+    end
+  end
 end
